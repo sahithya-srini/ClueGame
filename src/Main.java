9f718f9195630f0800fc461adcc56d1a6cf954f0
sahithya-srini <sahithya.srini@gmail.com>
2025-05-13 17:44:47 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,33 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
-        int[][] boardArr = new int[12][12];
 
+        ArrayList<String> roomList = new ArrayList<>();
+        roomList.add("Room 1");
+        roomList.add("Room 2");
+        roomList.add("Room 3");
+        roomList.add("Room 4");
+        roomList.add("Room 5");
+        roomList.add("Room 6");
+        String roomKilled = roomList.remove((int)(Math.random() * 6));
+
+        ArrayList<String> suspectList = new ArrayList<>();
+        suspectList.add("Suspect 1");
+        suspectList.add("Suspect 2");
+        suspectList.add("Suspect 3");
+        suspectList.add("Suspect 4");
+        String killer = suspectList.remove((int)(Math.random() * 4));
+
+        System.out.println(("Killer : " + killer + "\nRoom Killed: " + roomKilled));
+
+
+
+        int[][] boardArr = new int[12][12];
         Board board = new Board();
         board.start(primaryStage);
 
